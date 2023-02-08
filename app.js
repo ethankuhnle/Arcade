d1fa78b2a615c9293ecce4c777335596a73987a1
ekuhnle <epk800@gmail.com>
2023-02-08 15:21:26 -0500

    update

---
+++ app.js
@@ -0,0 +1,10 @@
+let cell1 = document.getElementById(cell1);
+let cell2 = document.getElementById(cell2);
+let cell3 = document.getElementById(cell3);
+let cell4 = document.getElementById(cell4);
+let cell5 = document.getElementById(cell5);
+let cell6 = document.getElementById(cell6);
+let cell7 = document.getElementById(cell7);
+let cell8 = document.getElementById(cell8);
+let cell9 = document.getElementById(cell9);
+
